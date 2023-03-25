90a89a9c405927600a42292f40d7d520a169e69a
adam_ <adam_@LAPTOP-91JU9GJ0>
2023-03-25 19:47:49 +0100

    Add project files.

---
+++ OnMeasureBug/MyButton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnMeasureBug
+{
+    public class MyButton : Button
+    {
+        protected override SizeRequest OnMeasure(double widthConstraint, double heightConstraint)
+        {
+            //some logic
+            {
+                int i = 0;
+                i++;
+                Debug.WriteLine("This method never fires. It fires in Xamarin, but it doesn't fire in MAUI. " +
+                    "I hope you will fix it as soon as possible, because it's almost impossible to create advanced custom controls now."+
+                    "I can't migrate my project to MAUI from Xamarin.");
+
+            }
+            return base.OnMeasure(widthConstraint, heightConstraint);
+        }
+    }
+}
---
+++ OnMeasureBug/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace OnMeasureBug;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
